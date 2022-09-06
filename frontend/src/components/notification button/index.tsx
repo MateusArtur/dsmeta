61b80072e748dd5944251fb0ced9ebcaaa0d6c94
Mateus Artur <artur.corseg@gmail.com>
2022-09-06 14:33:20 -0300

    UseState

--- frontend/src/components/notification button/index.tsx
+++ frontend/src/components/notification button/index.tsx
@@ -1,6 +1,6 @@
-import icon from '../../assets/img/notif-icon.svg'
+import icon from '../../assets/img/notif-icon.svg';
 
-import '../../components/notification button/styles.css'
+import '../../components/notification button/styles.css';
 
 function NotificationButton() {
     return (
@@ -10,4 +10,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
