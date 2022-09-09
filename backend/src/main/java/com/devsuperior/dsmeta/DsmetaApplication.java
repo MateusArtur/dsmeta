5acfe4c9571ad806e945a5c31867ed1f969aa056
Mateus Artur <artur.corseg@gmail.com>
2022-09-09 12:06:05 -0300

    Data Base

--- backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -1,4 +1,4 @@
-package Projeto.da.semana.spring.react;
+package com.devsuperior.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
